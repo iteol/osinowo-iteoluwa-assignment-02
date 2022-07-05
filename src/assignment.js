cbdedf646b7dd5df4fec5d001fa3ec291f8be53b
Iteoluwakiisi osinowo <105950462+iteol@users.noreply.github.com>
2022-07-05 14:10:54 +0000

    check challenge 1 & challenge 2

--- src/assignment.js
+++ src/assignment.js
@@ -4,34 +4,28 @@ const assignment = {};
 // ========================
 
 
-/* *
+/**
  * Challenge - 1
- * calculate and return the sum of the numbers in an  array.
-
+ * 
+ * Calculate and return the sum of the numbers in an array.
  * If you did Challenge - 1, remove the comment in the line just after this function
  * 
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-
-let arrayOfNumbers = [1,2,3,4,5,6,7,8,9,10]
-
-
 function sumOfNumbers(arrayOfNumbers) {
-   sum = 0;
-  for (let i=0; i < arrayOfNumbers.length; i++) {
-  sum += arrayOfNumbers[i]
-
   
+    //return arrayOfNumbers.length;
+  sum = 0
+
+  for (let i=0; i< arrayOfNumbers.length; i++) {
+    sum += arrayOfNumbers[i];
   }
   return sum;
-//return arrayOfNumbers.length;
-  
-  
 }
-  console .log(arrayOfNumbers.length)
-console . log (sumOfNumbers(arrayOfNumbers))
-assignment.sumOfNumbers = sumOfNumbers;
+// console .log (arrayOfNumbers . length)
+// console.log(sumOfNumbers(arrayOfNumbers))
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -42,19 +36,22 @@ assignment.sumOfNumbers = sumOfNumbers;
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
-
 function countEvenNumbers(arrayOfNumbers) {
-  //return arrayOfNumbers
+  
+   // return arrayOfNumbers.length;
   count = 0
-  for (let i =0 ; i<arrayOfNumbers. length;i++) {
-    if (arrayOfNumbers[i] % 2 === 0) {
-      count++;
+  for (let i=0; i< arrayOfNumbers.length; i++){
+    
+  
+    if (arrayOfNumbers[i] % 2 ===0){
+
+  count++
   }
 }
-    return count;
+  return count;
 }
-console. log(countEvenNumbers(arrayOfNumbers))
- assignment.countEvenNumbers = countEvenNumbers;
+// console.log(countEvenNumbers(arrayOfNumbers))
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
